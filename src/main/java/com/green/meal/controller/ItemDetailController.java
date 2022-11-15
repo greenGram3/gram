b501707b83db3922e59c334b0b281b0109a37563
jmh3084 <jm3084@naver.com>
2022-11-15 17:43:08 +0900

    itemDetailController 첫글자 소문자에서 대문자 변경 ItemDetailController에 buy메서드 삽입 (작업 미완료) payment.jsp 삽입 (작업 미완료) paymentConfirm.jsp (작업 미완료)

--- src/main/java/com/green/meal/controller/ItemDetailController.java
+++ src/main/java/com/green/meal/controller/ItemDetailController.java
@@ -5,10 +5,11 @@ import com.green.meal.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class itemDetailController {
+public class ItemDetailController {
     @Autowired
     ItemService itemService;
 
@@ -50,6 +51,20 @@ public class itemDetailController {
     @RequestMapping(value="/deliInfo")
     public String itemDetailPage(Model m) {
         return "itemDelyInfo";
+    }
+
+
+    //    ========= 상세페이지->결제페이지 작업 중 코드(정무혁) =============
+
+    @RequestMapping("/buy")
+    public String buy() {
+
+        return "payment";
+    }
+
+    @PostMapping("/paymentConfirm")
+    public String paymentConfirm() {
+        return "paymentConfirm";
     }
 
 } //itemDetailController
