a9c4e7041ae147c404af8a61c0ccaf016a8a8a49
oceanagape <bielman@nate.com>
2022-11-15 11:08:37 +0900

    DelyService 고침

--- src/main/java/com/green/meal/service/DelyService.java
+++ src/main/java/com/green/meal/service/DelyService.java
@@ -1,38 +1,24 @@
 package com.green.meal.service;
 
 import com.green.meal.domain.DeliveryVO;
-import com.green.meal.mapper.DeliveryMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.apache.ibatis.annotations.Mapper;
+
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
-@Service
-public class DelyService {
 
-    private final DeliveryMapper mapper;
+public interface DelyService {
 
-    public List<DeliveryVO> selectDelivery(String userId){
-        return mapper.selectDelivery(userId);
-    }
+     List<DeliveryVO> selectDelivery(String userId);
 
-    public int delyProc(DeliveryVO dely){
-        return mapper.delyProc(dely);
-    }
+    int delyProc(DeliveryVO dely);
 
-    public int updateDelivery(DeliveryVO dely0, DeliveryVO dely1){
-        Map map = new HashMap();
-        map.put("dely0", dely0);
-        map.put("dely1", dely1);
-        return mapper.updateDelivery(map);
-    }
+    int updateDelivery(DeliveryVO dely0, DeliveryVO dely1);
+    public int deleteDelivery(DeliveryVO dely);
 
-    public int deleteDelivery(DeliveryVO dely){
-        return mapper.deleteDelivery(dely);
-    }
+    int updateDelivery2(String userId);
 
+    int selectBase(String userId);
 }
